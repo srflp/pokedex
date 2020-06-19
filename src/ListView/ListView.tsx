585d50f9990d861fc8c2179fed1fd1f269ea8d96
srflp <sauer.filip@gmail.com>
2020-06-19 23:22:29 +0200

    Post-rewrite code cleanup

--- src/ListView/ListView.tsx
+++ src/ListView/ListView.tsx
@@ -2,19 +2,13 @@ import React from "react";
 import TypeFilter from "./TypeFilter";
 import Search from "./Search";
 import PokemonBrowser from "./PokemonBrowser/PokemonBrowser";
-import { PagesProvider } from "./usePages";
-import { SearchProvider } from "./useSearch";
 
 const ListView: React.FC = () => {
   return (
     <>
       <TypeFilter />
-      <SearchProvider>
-        <Search />
-        <PagesProvider>
-          <PokemonBrowser />
-        </PagesProvider>
-      </SearchProvider>
+      <Search />
+      <PokemonBrowser />
     </>
   );
 };
